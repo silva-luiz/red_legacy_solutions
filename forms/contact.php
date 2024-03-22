a51087716e7f209de94332fa1a16f9962ca6a53c
Luiz Silva <luiz.silva@starone.one>
2024-03-22 14:57:19 +0000

    contact php added

--- forms/contact.php
+++ forms/contact.php
@@ -1,28 +1,41 @@
 <?php
-if(isset($_POST['email']) && !empty['email']) {
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
 
-$name = addslashes($_POST['name']);
-$email = addslashes($_POST['email']);
-$cnpj = addslashes($_POST['cnpj']);
-$phone = addslashes($_POST['phone']);
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'info@redlegacysolutions.com.com';
 
-$to = "luiz.xd14@hotmail.com";
-$subject = "Website form";
-$body = "Nome: ".$name. "\r\n".
-        "CNPJ: ".$cnpj. "\r\n".
-        "Telefone: ".$phone;
-$header = "From: luizera169@gmail.com"."\r\n".
-          ."Replay-To:".$email."\r\n".
-          ."X=Mailer:PHP/".phpversion();
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
 
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
 
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
 
-if(mail($to, $subject, $body, $header)){
-  echo("Email enviado com sucesso!");
-}else{
-  echo("Ocorreu um erro no envio do email");
-}
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
 
-}
-
+  echo $contact->send();
 ?>
---
+++ send.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+
+if(isset($_POST['send'])) {
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'info@redlegacysolutions.com';                     //SMTP username
+    $mail->Password   = 'Redem@il93!';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('info@redlegacysolutions.com', 'Mailer');
+    $mail->addAddress('info@redlegacysolutions.com', 'Red Legacy Solutions');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Contato via Forms do Site';
+
+    $body = "Mensagem enviada atraves do site. Informacoes do Remetente:<br>
+      Nome:". $_POST['name']."<br>
+      Email:". $_POST['email']."<br>
+      CNPJ: ". $_POST['cnpj']."<br>
+      Telefone: ".$_POST['phone']."<br>";                                  
+
+      $mail->Body    = $body;
+
+    $mail->send();
+    echo 'Email enviado com sucesso.';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+}
